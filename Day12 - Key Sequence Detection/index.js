e2f09eb8486a03de23809c2cb663e1851b318c23
tasarozan <tasarozan@gmail.com>
2021-05-02 22:18:00 +0300

    Add twelfth project

---
+++ Day12 - Key Sequence Detection/index.js
@@ -0,0 +1,12 @@
+const pressed = []
+const secretCode = 'freq'
+window.addEventListener('keyup', (e) => {
+  console.log(e.key)
+  pressed.push(e.key)
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+  console.log(pressed)
+  if (pressed.join('').includes(secretCode)) {
+    console.log('DING DING')
+    cornify_add()
+  }
+})
